f311a665b117614997a5beac44182195ec8157fd
Олег Спас <spas.oleh@gmail.com>
2023-03-03 11:04:39 +0100

    New code

--- Lab_2/Lab_2.cpp
+++ Lab_2/Lab_2.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <vector>
 #include "Lab_2.h"
+
 using namespace std;
 void fill_with_random_numbers_and_print(const int& n, int  a[])
 {
@@ -12,24 +13,14 @@ void fill_with_random_numbers_and_print(const int& n, int  a[])
     }
     cout << endl;
 }
-int find_position_of_max_element(const int& n, int  a[])
-{
-    int position_of_max_element = 0;
-    
-    for (int i = 0; i < n; i++) {
-        if (a[i] > a[position_of_max_element]) {
-            position_of_max_element = i;
-        }
-    }
-    return position_of_max_element;
-}
 pair<int, int> find_positions_of_two_zeros(const int& n, int  a[])
 {
     int first_zero_position = -1;
     int second_zero_position = -1;
     for (int i = 0; i < n; i++) {
-        if (a[i] == 0) {
+        if (a[i] == 0  && first_zero_position == -1) {
             first_zero_position = i;
+            continue;
         }
         if (a[i] == 0 && first_zero_position !=-1) {
             second_zero_position = i;
@@ -37,7 +28,7 @@ pair<int, int> find_positions_of_two_zeros(const int& n, int  a[])
         }
     }
     if (first_zero_position == -1 || second_zero_position == -1) {
-        cout << "Not enough null elements found" << endl;
+        cout << "Not enough zero elements found" << endl;
     }
     return pair<int, int>(first_zero_position, second_zero_position);
 }
@@ -47,7 +38,7 @@ int find_product_between_two_positions(std::pair<int, int>& positions, int  a[10
         cout << "Positions out of border: " << endl;
         return -1;
     }
-    if (positions.first < positions.second) {
+    if (positions.first > positions.second) {
         cout << "First position must be before second: " << endl;
         return -1;
     }
@@ -82,14 +73,15 @@ void sort_for_variant_7(const int& n, int  a[]) {
 int main() {
     setlocale(LC_ALL, "Ukrainian");
     srand(time(NULL));
-    const int n = 10;
+    const int n = 20;
     int a[n];
     fill_with_random_numbers_and_print(n, a);
-    int max_index = find_position_of_max_element(n,a);
-    pair<int, int> zeros_positions = find_positions_of_two_zeros(n, a);
+    int max_index = max_element(begin(a),end(a))-begin(a);
 
     cout << "Max element index: " << max_index << endl;
     cout << "Product of elements between first and second zeros: ";
+    pair<int, int> zeros_positions = find_positions_of_two_zeros(n, a);
+
     cout << find_product_between_two_positions(zeros_positions,a,n)<< endl;
     sort_for_variant_7(n, a);
     cout << "Sorted array: ";
@@ -98,5 +90,7 @@ int main() {
         cout << a[i] << " ";
     }
     cout << endl;
+
+
     return 0;
 }
